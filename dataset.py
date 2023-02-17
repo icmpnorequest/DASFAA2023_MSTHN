88f4a766cac948665ee967b8816e9d61c3d67a36
icmpnorequest <sculyt10@gmail.com>
2023-02-17 11:55:18 +0800

    add comment

--- dataset.py
+++ dataset.py
@@ -6,12 +6,13 @@ from torch.utils.data import Dataset, DataLoader
 class POIDataset(Dataset):
     def __init__(self, data_filename, pois_coos_dict, num_users, num_pois, max_seq_len, padding_idx, eta,
                  distance_threshold, distance_type, conv, device):
-
+        # load data
         self.data = load_list_with_pkl(data_filename)
         self.sessions_dict = self.data[0]
         self.labels_dict = self.data[1]
         self.pois_coos_dict = pois_coos_dict
 
+        # definition
         self.num_users = num_users
         self.num_pois = num_pois
         self.num_sessions = get_num_sessions(self.sessions_dict)
@@ -23,17 +24,21 @@ class POIDataset(Dataset):
         self.conv = conv
         self.device = device
 
+        # generate sequence and geographical sequence for users
         self.unique_users_seqs_dict, self.unique_users_seqs_lens_dict = get_unique_seqs_for_sessions(self.sessions_dict)
         self.users_seqs_dict, self.users_reverse_seqs_dict = process_users_seqs(self.unique_users_seqs_dict, self.padding_idx, self.max_seq_len)
         self.users_seqs_masks_dict = gen_users_seqs_masks(self.users_seqs_dict, self.padding_idx)
         self.users_geo_seqs_dict = gen_geo_seqs_adjs_dict(self.users_seqs_dict, self.pois_coos_dict, self.max_seq_len, self.padding_idx, eta, distance_threshold, distance_type)
 
+        # generate local graph
         self.A = gen_sparse_A(self.users_seqs_dict, num_users, num_pois, padding_idx)
         self.G = gen_local_graph(self.A)
 
+        # generate global hypergraph
         self.H = gen_sparse_H(self.sessions_dict, num_pois, self.num_sessions, self.padding_idx)
         self.HG = gen_HG_from_sparse_H(self.H, conv)
 
+        # transform csr matrix to tensor
         self.G = transform_csr_matrix_to_tensor(self.G)
         self.HG = transform_csr_matrix_to_tensor(self.HG)
         self.G = self.G.to(self.device)
